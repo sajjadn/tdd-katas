c35f93135f3b5a152d62007f6aec4c204e658dc4
Sajjad Nazrulla <sajjad.nazrulla@gmail.com>
2022-07-10 23:10:07 +0530

    Completed single transaction processing

--- transaction_pipeline/src/store_revenue.py
+++ transaction_pipeline/src/store_revenue.py
@@ -1,3 +1,7 @@
 
 class StoreRevenue:
     store_id: int
+
+    def __init__(self, store_id) -> None:
+        super().__init__()
+        self.store_id = store_id
--- transaction_pipeline/src/transaction_pipeline.py
+++ transaction_pipeline/src/transaction_pipeline.py
@@ -3,5 +3,5 @@ from src.store_revenue import StoreRevenue
 
 
 def run() -> int:
-    reporting_db.save_records(StoreRevenue())
+    reporting_db.save_records(StoreRevenue(1))
     return 0
