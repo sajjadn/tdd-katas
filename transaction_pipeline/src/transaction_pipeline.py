dd37f3d2bf6aea99191c699efb7c54ce7e44b62d
Sajjad Nazrulla <sajjad.nazrulla@gmail.com>
2022-07-10 23:29:12 +0530

    Fix first test that failed

--- transaction_pipeline/src/transaction_pipeline.py
+++ transaction_pipeline/src/transaction_pipeline.py
@@ -3,9 +3,11 @@ from src.store_revenue import StoreRevenue
 
 
 def run() -> int:
-    transactions: list = transaction_api_client.fetch_transactions()["transactions"]
-    store_revenues = []
-    for transaction in transactions:
-        store_revenues.append(StoreRevenue(transaction["storeId"]))
-    reporting_db.save_records(store_revenues)
+    transaction_response: list = transaction_api_client.fetch_transactions()
+    if transaction_response and "transactions" in transaction_response:
+        transactions = transaction_response["transactions"]
+        store_revenues = []
+        for transaction in transactions:
+            store_revenues.append(StoreRevenue(transaction["storeId"]))
+        reporting_db.save_records(store_revenues)
     return 0
